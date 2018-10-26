8a9183e7a873d0a201a11a6851d323963c97b7d2
Chris Tsang (ASD) <chris.tsang@asdtech.local>
2018-10-26 14:56:14 +0800

    Fix bug: use time() and localtime() function to replace __DATE__ and __TIME__ macro

--- main.c
+++ main.c
@@ -4,9 +4,17 @@
 
 #define BUILD_NUM_STR	"BUILD_NUMBER"
 #define BUILD_DATE_STR	"BUILD_DATE"
-#define MONTH_STR	"MM"
-#define DAY_STR		"DD"
-#define YEAR_STR	"YY"
+#define BUILD_TIME_STR	"BUILD_TIME"
+
+#define MONTH_STR		"MM"
+#define DAY_STR			"DD"
+#define YEAR_STR		"YY"
+
+#define HOUR_STR		"HR"
+#define MINUTE_STR		"MIN"
+#define SECOND_STR		"SEC"
+
+#include <time.h>
 
 int ConfigBuildDate(char *argv[], char *DATE_CODE)
 {
@@ -16,6 +24,12 @@ int ConfigBuildDate(char *argv[], char *DATE_CODE)
 	char *foundstr;
 	int temp;
 	int ret = 0;
+
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    char fileDateCode[32];
+
+    sprintf(fileDateCode, "%d %d %d", tm.tm_mon + 1, tm.tm_mday, tm.tm_year - 100);
 
     fp=fopen(argv[1], "r+");
 	if(fp==NULL)
@@ -34,7 +48,7 @@ int ConfigBuildDate(char *argv[], char *DATE_CODE)
             {
                 printf("Build Date found - %s[%d]\n", DATE_CODE, strlen(DATE_CODE));
 
-                if (strcmp(DATE_CODE, __DATE__) == 0) //Date match, inc build number
+                if (strcmp(DATE_CODE, fileDateCode) == 0) //Date match, inc build number
                 {
                     printf("Date match, inc build number\n");
                     ret = 1;
@@ -47,7 +61,48 @@ int ConfigBuildDate(char *argv[], char *DATE_CODE)
 
 			temp = ftell(fp) - strlen(foundstr)+strlen(BUILD_DATE_STR) ;
 			fseek(fp, (long)temp, SEEK_SET);
-			fprintf(fp,"	 \"%s\" \n", __DATE__);
+			fprintf(fp,"	 \"%d %d %d\" \n", tm.tm_mon + 1, tm.tm_mday, tm.tm_year - 100);
+			fflush(fp);
+			fseek(fp, 0L, SEEK_END);
+		}
+		fgets(str, 127, fp);
+	}
+	fclose(fp);
+
+	return ret;
+}
+
+int ConfigBuildTime(char *argv[], char *TIME_CODE)
+{
+	FILE *fp;
+	char str[128];
+
+	char *foundstr;
+	int temp;
+	int ret = 0;
+
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    char fileTimeCode[32];
+
+    sprintf(fileTimeCode, "%02d:%02d:%02d", tm.tm_hour, tm.tm_min, tm.tm_sec);
+
+    fp=fopen(argv[1], "r+");
+	if(fp==NULL)
+	{
+		printf("error opening file %s. \n", argv[1]);
+		exit(1);
+	}
+
+	while(!feof(fp))
+	{
+		if((foundstr=strstr(str, BUILD_TIME_STR)) != NULL) /* if string found */
+		{
+			sscanf(foundstr, "%*s \"%[^\t\n\"]s", TIME_CODE);
+			strcpy(TIME_CODE, fileTimeCode);
+			temp = ftell(fp) - strlen(foundstr)+strlen(BUILD_TIME_STR) ;
+			fseek(fp, (long)temp, SEEK_SET);
+			fprintf(fp,"	 \"%s\" \n", fileTimeCode);
 			fflush(fp);
 			fseek(fp, 0L, SEEK_END);
 		}
@@ -148,11 +203,11 @@ int ConfigVersionNumber(char *argv[], char *DATE_CODE)
     date = strtok_r(NULL, " \t\n", &saveptr);
     year = strtok_r(NULL, " \t\n", &saveptr);
 
-    // printf("month[%d] = %s\n", strlen(month), month);
-    // printf("date[%d] = %s\n", strlen(date), date);
-    // printf("year[%d] = %s\n", strlen(year), year);
+    //printf("month[%d] = %s\n", strlen(month), month);
+    //printf("date[%d] = %s\n", strlen(date), date);
+    //printf("year[%d] = %s\n", strlen(year), year);
 
-	strncpy(YY, year+2, 2);
+	strcpy(YY, year);
     printf("YY[%d] = %s\n", strlen(YY), YY);
 	ConfigVersionField(argv, YY, YEAR_STR);
 
@@ -209,10 +264,32 @@ int ConfigVersionNumber(char *argv[], char *DATE_CODE)
 	{
 		strcpy(MM, "12");
 	}
+	else
+    {
+        strcpy(MM, month);
+    }
     printf("MM[%d] = %s\n", strlen(MM), MM);
 	ConfigVersionField(argv, MM, MONTH_STR);
 
     return 0;
+}
+
+int ConfigVersionTime(char *argv[], char *DATE_CODE)
+{
+	char *hour, *minute, *second, *saveptr;
+    hour = strtok_r(DATE_CODE, " :\t\n", &saveptr);
+    minute = strtok_r(NULL, " :\t\n", &saveptr);
+    second = strtok_r(NULL, " :\t\n", &saveptr);
+
+    //printf("hour[%d] = %s\n", strlen(hour), hour);
+    //printf("minute[%d] = %s\n", strlen(minute), minute);
+    //printf("second[%d] = %s\n", strlen(second), second);
+
+	ConfigVersionField(argv, hour, HOUR_STR);
+	ConfigVersionField(argv, minute, MINUTE_STR);
+	ConfigVersionField(argv, second, SECOND_STR);
+
+	return 0;
 }
 
 
@@ -226,12 +303,14 @@ int main(int argc, char *argv[])
 		printf("  The number following will be incremented by 1 and saved. \n\n\n");
 		exit(1);
 	}
-    char DATE_CODE[128] = "";
+    char DATE_CODE[128] = "", TIME_CODE[128] = "";
+	int isTimeMatch = ConfigBuildTime(argv, TIME_CODE);
 	int isDateMatch = ConfigBuildDate(argv, DATE_CODE);
     IncBuildNumber(argv, isDateMatch);
 
-    // printf("DATECODE = %s\n", DATE_CODE);
+    //printf("DATECODE = %s\n", DATE_CODE);
     ConfigVersionNumber(argv, DATE_CODE);
+    ConfigVersionTime(argv, TIME_CODE);
 
 	return(0);
 }
